e070bbe0be49630ed3df6d714a53c09532b822dc
jmandujano24 <jmandujano@pucp.pe>
2026-05-05 06:03:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
-from flask import Flask, request
+from flask import Flask
 import subprocess
-import os
 
 app = Flask(__name__)
-
-SECRET = os.getenv("WEBHOOK_SECRET", "")
-
 
 @app.route("/")
 def home():
     return "Watchlist bot alive"
 
-
 @app.route("/run")
 def run_bot():
-    if SECRET:
-        if request.args.get("key") != SECRET:
-            return "unauthorized", 401
-
     subprocess.run(["python", "main.py"])
     return "ok", 200
